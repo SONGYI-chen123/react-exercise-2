a66a7c845223fe52fa90a4ab03782575a494375c
SONGYI-chen123 <69594015+SONGYI-chen123@users.noreply.github.com>
2020-10-07 20:00:17 +0800

    Update App.js
    finished

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import Header from './components/Header';
 import Products from './components/products';
 
 class App extends Component {
+  
+    constructor(){
+      super();
+      this.state = {
+        count: 0
+      }
+    }
+
+
+  AddToShoppingCart = () =>{
+    this.setState({
+      count: this.state.count + 1,
+  });
+  }
+  
   render() {
     return (
       <main className="app">
-        <Header />
-        <Products />
+       <Header count={this.state.count}/>
+       <Products  AddToShoppingCart={this.AddToShoppingCart}/>
       </main>
     );
   }
